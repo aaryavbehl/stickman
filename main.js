dd96d9c1f31185987019db8ec69002a8739e05d4
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-08 17:43:59 +0530

    modified:   main.js

--- main.js
+++ main.js
@@ -85,9 +85,187 @@ function resetGame(){
 }
 
 function generateTree() {
-    Math.floor(Math.random() * (maximumGap - minimumGap));
+    const minimumGap = 30;
+    const maximumGap = 150;
+    const lastTree = trees[trees.length - 1];
+    let furthestX = lastTree ? lastTree.x : 0;
+    const x=
+        furthestX +
+        minimumGap +
+        Math.floor(Math.random() * (maximumGap - minimumGap));
     const treeColors = ["#6D8821", "#8FAC34", "#98B333"];
     const color = treeColors[Math.floor(Math.random() * 3)];
     trees.push([x,color]);
 }
 
+function generatePlatform(){
+    const minimumGap = 40;
+    const maximumGap = 200;
+    const minimumWidth = 20;
+    const maximumWidth = 100;
+    const lastPlatform = platforms[platforms.length - 1];
+    let furthestX = lastPlatform.x + lastPlatform.w;
+    const x =
+      furthestX +
+      minimumGap +
+      Math.floor(Math.random() * (maximumGap - minimumGap));
+    const w =
+      minimumWidth + Math.floor(Math.random() * (maximumWidth - minimumWidth));
+    platforms.push({x, w});
+}
+
+resetGame();
+
+window.addEventListener("keydown", function (event){
+    if (event.key == " "){
+        event.preventDefault();
+        resetGame();
+        return;
+    }
+});
+
+window.addEventListener("mousedown", function (event){
+    if (phase == "waiting"){
+        lastTimestamp = undefined;
+        introductionElement.style.opacity = 0;
+        phase = "stretching";
+        window.requestAnimationFrame(animate);
+    }
+});
+
+window.addEventListener("mouseup", function (event){
+    if (phase == "stretching"){
+        phase = "turning";
+    }
+});
+
+window.addEventListener("resize", function (event){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    draw();
+});
+
+window.requestAnimationFrame(animate);
+
+function animate(timestamp){
+    if(!lastTimestamp){
+        lastTimestamp = timestamp;
+        window.requestAnimationFrame(animate);
+        return;
+    }
+    switch (phase){
+        case "waiting":
+            return;
+        case "stretching": {
+            sticks.last().length += (timestamp - lastTimestamp) / stretchingSpeed;
+            break;
+        }
+        case "turning": {
+            sticks.last().rotation += (timestamp - lastTimestamp) / turningSpeed;
+
+            if(sticks.last().rotation > 90){
+                sticks.last().rotation = 90;
+
+                const [nextPlatform, perfectHit] = thePlatformTheStickHits();
+                if(nextPlatform){
+                    score += perfectHit ? 2 : 1;
+                    scoreElement.innerText = score;
+                    
+                    if(perfectHit){
+                        perfectElement.style.opacity = 1;
+                        setTimeout(() => (perfectElement.style.opacity = 0), 1000);
+                    }
+                    generatePlatform();
+                    generateTree();
+                    generateTree();
+                }
+
+                phase = "walking";
+            }
+
+            break;
+        }
+        case "walking": {
+            heroX += (timestamp - lastTimestamp) / walkingSpeed;
+
+            const [nextPlatform] = thePlatformTheStickHits();
+            if (nextPlatform){
+                const maxHeroX = nextPlatform.x + nextPlatform.w - heroDistanceFromEdge;
+                if (heroX > maxHeroX){
+                    heroX = maxHeroX;
+                    phase = "transitioning";
+                }
+            } else {
+                const maxHeroX = sticks.last().x + sticks.last().length + heroWidth;
+                if (heroX > maxHeroX) {
+                    heroX = maxHeroX;
+                    phase = "falling";
+                }
+            }
+
+            break;
+        }
+        
+        case "transitioning": {
+            sceneOffset += (timestamp - lastTimestamp) / transitioningSpeed;
+            const [nextPlatform] = thePlatformTheStickHits();
+            if (sceneOffset > nextPlatform.x + nextPlatform.w - paddingX){
+                sticks.push({
+                    x: nextPlatform.x + nextPlatform.w,
+                    length: 0,
+                    rotation: 0
+                });
+
+                phase = "waiting";
+            }
+
+            break;
+
+        }
+        case "falling":{
+            if (sticks.last().rotation < 180)
+                sticks.last().rotation += (timestamp - lastTimestamp) / turningSpeed;
+            heroY += (timestamp - lastTimestamp) / fallingSpeed;
+            const maxHeroY = 
+                platformHeight + 100 + (window.innerHeight - canvasHeight) / 2;
+            if (heroY > maxHeroY) {
+                restartButton.style.display = "block";
+                return;
+            }
+
+            break;
+
+        }
+
+        default:
+            throw Error("Wrong Phase");
+
+    }
+
+    draw();
+    window.requestAnimationFrame(animate);
+
+    lastTimestamp = timestamp;
+}
+
+function thePlatformTheStickHits(){
+
+    if (sticks.last().rotation != 90)
+        throw Error(`Stick is ${sticks.last().rotation}°`);
+    const stickFarX = sticks.last().x + sticks.last().length;
+
+    const platformTheStickHits = platforms.find(
+        (platform) => platform.x < stickFarX && stickFarX < platform.x + platform.w
+    );
+
+    if (
+        platformTheStickHits &&
+        platformTheStickHits.x + platformTheStickHits.w / 2 - perfectAreaSize / 2 <
+          stickFarX &&
+        stickFarX <
+          platformTheStickHits.x + platformTheStickHits.w / 2 + perfectAreaSize / 2
+      )
+        return [platformTheStickHits, true];
+
+    return [platformTheStickHits, false];
+}
